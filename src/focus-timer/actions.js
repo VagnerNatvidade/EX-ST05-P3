d57e9b746165e62ef5383bee73914cf0d434726b
vagnerNatividade <vagnerflorindojunior@gmail.com>
2023-11-20 18:32:53 -0300

    Aula 14 - Conectando as primeiras ações

---
+++ src/focus-timer/actions.js
@@ -0,0 +1,11 @@
+export function toggleRunning() {
+  console.log("toggleRunning");
+}
+
+export function set() {
+  console.log("set");
+}
+
+export function toggleMusic() {
+  console.log("toggleMusic");
+}
